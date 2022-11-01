1e58d4ef4b9408c4f6d2aa68ee397619ae579ab3
pamacs <gyorgy.andorka@protonmail.com>
2022-11-01 18:50:53 +0100

    improv!: stuff (too early for atomic commits)

--- README.md
+++ README.md
@@ -11,30 +11,30 @@ AFAIK, the basic idea first appeared in
 [vim-sneak](https://github.com/justinmk/vim-sneak)'s predecessors. (The feature
 is coincidentally called "leaping motions" there, no kidding.)
 
-It's not just number of keystrokes that matter here, but the potentially more
-intuitive workflow achieved through these higher abstractions, that are
+It's not just the number of keystrokes that matter here, but the potentially
+more intuitive workflow achieved through these higher abstractions, that are
 nevertheless obvious extensions of Vim's grammar. As usual, the aim is to
-sharpen the saw; there are no big list of new commands to learn, except for an
-affix that can be added to all existing text objects. `carb<leap>` ("change
+sharpen the saw; there are no big list of new commands to learn, except for two
+affixes that can be added to all existing text objects. `carb<leap>` ("change
 around remote block [marked by leap motion]") in no time will be just as natural
 as targets.vim's `canb` ("change around next block").
 
 ## Usage
 
-The "spooky" text objects automatically invoke Leap; after e.g. `yarw`, start
-typing the 2-character search pattern, and select the target as you would
-usually do. The difference is that instead of jumping there, the word will be
-yanked.
+Leap is automatically invoked once the text object is specified; after e.g.
+`yarw`, start typing the 2-character search pattern, and select the target as
+you would usually do. The difference is that instead of jumping there, the word
+will be yanked.
 
 ## What are some fun things you can do with this?
 
-- Comment/delete/indent paragraphs without leaving your position (`darp<leap>`).
-- Fix a typo, even in another window, with a short, atomic command sequence
-  (`ciRw<leap><correction>`).
-- Operate on distant lines: `crr<leap>`.
-- Clone text objects with a few keystrokes by turning on auto-paste after
-  yanking (`yarp<leap>`).
-- Use `count`: e.g. `y3arw<leap>` yanks 3 words from the anchor point.
+- delete/fold/comment/etc. paragraphs without leaving your position
+  (`zfarp<leap>`).
+- Clone text objects in the blink of an eye, even from another window, by
+  turning on auto-paste after yanking (`yarp<leap>`).
+- Fix a typo with a short, atomic command sequence (`cimw<leap><correction>`).
+- Operate on distant lines: `drr<leap>`.
+- Use `count`: e.g. `y3rr<leap>` yanks 3 lines, just as `3yy` would do.
 
 ## Status
 
@@ -46,27 +46,57 @@ WIP - everything is experimental at the moment.
 
 ## Setup
 
-Spooky exposes only one convenience function. You can call it without arguments,
+`setup` creates all the necessary mappings - you can call it without arguments,
 if the defaults are okay:
 
 ```lua
 require('leap-spooky').setup {
   affixes = {
     -- These will generate mappings for all native text objects, like:
-    -- ir{obj}, ar{obj}, iR{obj}, aR{obj}, etc.
-    -- Special "remote" line objects will also be added, by repeating the
-    -- affixes. E.g. `yrr<leap>` will yank a line in the current window.
-    window       = 'r',
-    cross_window = 'R',
+    -- (ir|ar|iR|aR|im|am|iM|aM){obj}.
+    -- Special line objects will also be added, by repeating the affixes.
+    -- E.g. `yrr<leap>` and `ymm<leap>` will yank a line in the current
+    -- window.
+    -- You can also use 'rest' & 'move' as mnemonics.
+    remote   = { window = 'r', cross_window = 'R' },
+    magnetic = { window = 'm', cross_window = 'M' },
   },
   -- If this option is set to true, the yanked text will automatically be pasted
-  -- at the cursor position if the unnamed register is in use.
+  -- at the cursor position if the unnamed register is in use (and the object is
+  -- "non-magnetic").
   yank_paste = false,
-  -- Call-specific overrides for the Leap motion itself.
-  -- E.g.: opts = { equivalence_classes = {} }
-  opts = nil,
 }
 ```
+
+## Customisation
+
+Note: This is absolutely not stable API, just a current snapshot for people who
+would like to experiment.
+
+`spooky_action` returns a one-argument function that can be used as `leap`'s
+`action` parameter. That is, you have to call it when used in a mapping.
+
+The signature looks like: `spooky_action(action, {opts})`
+
+- `action`: a function returning a string to be passed to `:normal` (by default,
+  the expected action is a text object selection, like `viw`)
+- `opts.on_return`: like `action`, but the command is be executed after the
+  operation has been finished
+- `opts.keeppos`: if true, execute the action remotely (jump back afterwards)
+
+Example:
+
+```lua
+require('leap').leap {
+  target_windows = { vim.fn.win_getid() }
+  action = require('leap-spooky').spooky_action(
+    function () return "viw" end,
+    { keeppos = true, on_return = (vim.v.operator == 'y') and 'p', },
+  ),
+}
+```
+
+You can also check the source code for ideas, or if something is unclear.
 
 ## Planned features
 
@@ -76,7 +106,7 @@ require('leap-spooky').setup {
   highlight the active areas.
 
 - Label the text objects themselves (at least blocks, paragraphs, etc.), so that
-  you can immediately choose one, instead of having to specify the anchor point
-  with a default 2-char Leap motion.
+  you can immediately choose one, instead of having to specify the reference
+  point with a default 2-char Leap motion.
 
 - API for "spookifying" custom (non-native) text objects.
--- lua/leap-spooky.lua
+++ lua/leap-spooky.lua
@@ -1,63 +1,51 @@
 local api = vim.api
 
-local default_affixes = { 
-  window       = 'r',
-  cross_window = 'R',
-}
 
-local default_textobjects = {
-  'iw', 'iW', 'is', 'ip', 'i[', 'i]', 'i(', 'i)', 'ib',
-  'i>', 'i<', 'it', 'i{', 'i}', 'iB', 'i"', 'i\'', 'i`',
-  'aw', 'aW', 'as', 'ap', 'a[', 'a]', 'a(', 'a)', 'ab',
-  'a>', 'a<', 'at', 'a{', 'a}', 'aB', 'a"', 'a\'', 'a`',
-}
+local function get_motion_force()
+  local force = ""
+  local mode = vim.fn.mode(1)
+  if mode:sub(2) == 'oV' then force = "V"
+  elseif mode:sub(2) == 'o' then force = ""
+  end
+  return force
+end
 
 
-local function spooky_action(textobj, yank_paste)
+local function spooky_action(action, kwargs)
   return function (target)
-    local keeppos = vim.v.operator ~= 'c'
-    local yank_paste = yank_paste and vim.v.operator == 'y' and vim.v.register == "\""
+    local on_return = kwargs.on_return
+    local keeppos = kwargs.keeppos
     local saved_view = vim.fn.winsaveview()
-
-    -- Get forcing modifier.
-    local force = ""
-    local mode = vim.fn.mode(1)
-    if mode:sub(2) == 'oV' then force = "V"
-    elseif mode:sub(2) == 'o' then force = ""
-    end
-
     -- Handle cross-window operations.
     local source_win = vim.fn.win_getid()
     local cross_window = target.wininfo and target.wininfo.winid ~= source_win
-
     -- Set an extmark as an anchor, so that we can execute remote delete
     -- commands in the backward direction, and move together with the text.
     local ns = api.nvim_create_namespace("leap-spooky")
     local anchor = api.nvim_buf_set_extmark(0, ns, saved_view.lnum-1, saved_view.col, {})
 
     -- Jump.
-    if cross_window then
-      api.nvim_set_current_win(target.wininfo.winid)
-    end
+    if cross_window then api.nvim_set_current_win(target.wininfo.winid) end
     api.nvim_win_set_cursor(0, { target.pos[1], target.pos[2]-1 })
-    -- Execute.
-    vim.cmd("normal! v" .. vim.v.count1 .. textobj .. (textobj == "V" and "" or force))
+    -- Execute :normal action. (Intended usage: select some text object.)
+    vim.cmd("normal " .. action())  -- don't use bang - custom text objects should work too
+    -- (The operation itself will be executed after exiting.)
 
-    -- Things to do if staying in place.
-    if keeppos then
+    -- Follow-up:
+    if keeppos or on_return then
       api.nvim_create_autocmd('ModeChanged', {
-        pattern = '*:n',  -- on returning to Normal
+        pattern = '*:n',  -- trigger on returning to Normal
         once = true,
         callback = function ()
-          if cross_window then
-            api.nvim_set_current_win(source_win)
+          if keeppos then
+            if cross_window then api.nvim_set_current_win(source_win) end
+            vim.fn.winrestview(saved_view)
+            local anchorpos = api.nvim_buf_get_extmark_by_id(0, ns, anchor, {})
+            api.nvim_win_set_cursor(0, { anchorpos[1]+1, anchorpos[2] })
+            api.nvim_buf_clear_namespace(0, ns, 0, -1)  -- remove the anchor
           end
-          vim.fn.winrestview(saved_view)
-          local anchorpos = api.nvim_buf_get_extmark_by_id(0, ns, anchor, {})
-          api.nvim_win_set_cursor(0, { anchorpos[1]+1, anchorpos[2] })
-          api.nvim_buf_clear_namespace(0, ns, 0, -1)  -- remove the anchor
-          if yank_paste then
-            vim.cmd("normal! p")
+          if on_return then
+            vim.cmd("normal " .. on_return)
           end
         end,
       })
@@ -66,27 +54,48 @@ local function spooky_action(textobj, yank_paste)
 end
 
 
+local default_affixes = {
+  remote   = { window = 'r', cross_window = 'R' },
+  magnetic = { window = 'm', cross_window = 'M' },
+}
+
+local default_text_objects = {
+  'iw', 'iW', 'is', 'ip', 'i[', 'i]', 'i(', 'i)', 'ib',
+  'i>', 'i<', 'it', 'i{', 'i}', 'iB', 'i"', 'i\'', 'i`',
+  'aw', 'aW', 'as', 'ap', 'a[', 'a]', 'a(', 'a)', 'ab',
+  'a>', 'a<', 'at', 'a{', 'a}', 'aB', 'a"', 'a\'', 'a`',
+}
+
 local function setup(kwargs)
   local kwargs = kwargs or {}
-  local yank_paste = kwargs.yank_paste
   local affixes = kwargs.affixes
-  local opts = kwargs.opts
+  local yank_paste = kwargs.yank_paste
+
   local mappings = {}
-  for scope, key in pairs(affixes or default_affixes) do
-    for _, textobj in ipairs(default_textobjects) do
-      local ia = textobj:sub(1,1)
+  for kind, scopes in pairs(affixes or default_affixes) do
+    local keeppos = kind == 'remote'
+    for scope, key in pairs(scopes) do
+      for _, textobj in ipairs(default_text_objects) do
+        table.insert(mappings, {
+          scope = scope,
+          keeppos = keeppos,
+          lhs = textobj:sub(1,1) .. key .. textobj:sub(2),
+          action = function ()
+            return "v" .. vim.v.count1 .. textobj .. get_motion_force()
+          end,
+        })
+      end
+      -- Special case: remote lines.
       table.insert(mappings, {
-        lhs = ia .. key .. textobj:sub(2),
-        textobj = textobj,
         scope = scope,
+        keeppos = keeppos,
+        lhs = key .. key,
+        action = function ()
+          local n_js = vim.v.count1 - 1
+          return "V" .. (n_js > 0 and (tostring(n_js) .. "j") or "")
+        end,
       })
     end
-    -- Special remote line object.
-    table.insert(mappings, {
-      lhs = key .. key,
-      textobj = "V",
-      scope = scope,
-    })
   end
 
   for _, mapping in ipairs(mappings) do
@@ -97,17 +106,22 @@ local function setup(kwargs)
       elseif mapping.scope == 'cross_window' then
         target_windows = require'leap.util'.get_enterable_windows()
       end
+      local keeppos = mapping.keeppos
+      yank_paste = yank_paste and keeppos and vim.v.operator == 'y' and vim.v.register == "\""
       require'leap'.leap {
-        action = spooky_action(mapping.textobj, yank_paste),
+        action = spooky_action(mapping.action, {
+          keeppos = keeppos,
+          on_return = yank_paste and "p",
+        }),
         target_windows = target_windows,
-        opts = opts,
       }
     end)
   end
 end
 
 
-return { 
+return {
+  spooky_action = spooky_action,
   spookify = setup,
-  setup = setup
+  setup = setup,
 }
